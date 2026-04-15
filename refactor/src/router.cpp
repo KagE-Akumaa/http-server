f318534d0edee83d10e9d419e8b9702748d246ce
KagE-Akumaa <mukuljogi18@gmail.com>
2026-04-15 18:57:53 +0530

    router.hpp and router.cpp

---
+++ refactor/src/router.cpp
@@ -0,0 +1,41 @@
+#include "router.hpp"
+
+// NOTE: This function -> matches the correct route for the routehandler
+void Router::match(const Request &req, Response &res) {
+    // TODO: Response are not valid
+    std::string path = req.url;
+
+    auto dispatcher =
+        [&](std::function<void(const Request &, Response &)> &fn) {
+            fn(req, res);
+        };
+
+    if (req.method == "get") {
+        if (getRoutes.find(path) != getRoutes.end()) {
+            // Means we found the match
+            dispatcher(getRoutes[path]);
+        }
+        // FIX: return after giving a valid Response
+        return;
+    } else if (req.method == "post") {
+        if (postRoutes.find(path) != postRoutes.end()) {
+            // Means we found the match
+            dispatcher(postRoutes[path]);
+        }
+        // FIX: return after giving a valid Response
+        return;
+    }
+}
+
+// NOTE: Add the path to a designated get routehandler
+void Router::get(const std::string &path, RouteHandler rt) {
+    getRoutes[path] = rt;
+}
+
+// NOTE: Add the path to a designated post routehandler
+void Router::post(const std::string &path, RouteHandler rt) {
+    postRoutes[path] = rt;
+}
+
+// NOTE: This add the Middleware to the vector
+void Router::use(const Middleware mi) { middle.push_back(mi); }
