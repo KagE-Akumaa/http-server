da475df146c9a8a64079b9e490453d181a087f10
KagE-Akumaa <mukuljogi18@gmail.com>
2026-07-25 09:50:40 +0530

    update the structure

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "Http_Server.hpp"
+#include "json.hpp"
+#include "router.hpp"
+
+using json = nlohmann::json;
+int main() {
+        int port = 8989;
+        // NOTE: Now we need to pass the root directory to handle the static
+        // files
+        Router myRouter("/home/akumaa/Projects/http-server/assets");
+        myRouter.get("/", [](const Request &req, Response &res) {
+                res.status(200);
+                res.setContentType("application/json");
+
+                res.setBody(R"({
+  "users": [
+    {
+      "id": 1,
+      "name": "Mukul",
+      "email": "mukul@example.com"
+    },
+    {
+      "id": 2,
+      "name": "Aarav",
+      "email": "aarav@example.com"
+    },
+    {
+      "id": 3,
+      "name": "Riya",
+      "email": "riya@example.com"
+    }
+  ]
+})");
+        });
+
+        myRouter.post("/", [](const Request &req, Response &res) {
+                auto header = req.headers.find("content-type");
+
+                if (header != req.headers.end() &&
+                    header->second == "application/json") {
+                        try {
+                                json data = json::parse(req.body);
+                                std::string name = data["name"];
+                                std::string email = data["email"];
+
+                                res.status(200);
+                                res.setContentType("application/json");
+                                res.setBody(R"({"message" : "JSON recieved"})");
+                        } catch (const json::parse_error &e) {
+                                res.status(400);
+                                res.setContentType("application/json");
+                                res.setBody(R"({"error": "Invalid JSON"})");
+                        }
+                } else {
+                        res.status(415);
+                        res.setBody("Expected application/json");
+                }
+        });
+        HTTP_SERVER app(port, myRouter);
+        app.run();
+};
